5c3b33c92550bf4383fba24145eb151430ad646e
Hendrik Druse <h.druse@neusta.de>
2020-03-04 09:48:48 +0100

    Prefer library source for import
    prefer library source, because otherwise "part of"-files will lead to
    compile errors due to bad imports in the generated code

--- injectable_generator/lib/utils.dart
+++ injectable_generator/lib/utils.dart
@@ -2,16 +2,19 @@
 import 'package:analyzer/dart/element/element.dart';
 
 String getImport(Element element) {
-  //return early if element has no source
+  // prefer library source, because otherwise "part of"-files will lead to
+  // compile errors due to bad imports in the generated code
+  final source = element.librarySource ?? element.source;
 
-  if (element.source == null) {
+  //return early if element has no source
+  if (source == null) {
     return null;
   }
 
   // we don't need to import core dart types
   // or core flutter types
-  if (!element.source.isInSystemLibrary) {
-    final path = element.source.uri.toString();
+  if (!source.isInSystemLibrary) {
+    final path = source.uri.toString();
 
     return path;
   }
