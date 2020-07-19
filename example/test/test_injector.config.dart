cd9418145ea629b2921686ad2ee14489e651ef71
Milad Alakarie <m.alakarie@email.ly>
2020-07-19 21:45:09 +0200

    Release v1.0.0

---
+++ example/test/test_injector.config.dart
@@ -0,0 +1,18 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+// **************************************************************************
+// InjectableConfigGenerator
+// **************************************************************************
+
+import 'package:get_it/get_it.dart';
+import 'package:injectable/get_it_helper.dart';
+
+import 'test_service.dart';
+
+/// adds generated dependencies
+/// to the provided [GetIt] instance
+
+void $initGetIt(GetIt g, {String environment}) {
+  final gh = GetItHelper(g, environment);
+  gh.factory<TestService>(() => TestService());
+}
